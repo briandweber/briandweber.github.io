3db59a5836d82fd050601d55482398f3eccd6616
Brian <briandweber@gmail.com>
2024-05-29 10:50:19 -0500

    text edits

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = "My name is Brian D. Weber.  As a full stack software developer, I would be happy to talk to you about ways in which I can contribute to your goals.  Feel free to take a look at the projects portfolio";
+var txt = "My name is Brian D. Weber.  As a full stack software developer, I would be happy to talk to you about ways in which I can contribute to your goals.  Feel free to take a look at the projects portfolio.";
 var speed = 25; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
