1d890b979e328cc88b78a211735c0df045583518
Brian <briandweber@gmail.com>
2025-11-14 13:08:36 -0600

    added excalidraw wireframe

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,6 +6,7 @@ import gamesterLogo from './assets/images/gamesterLogo.png';
 import gamesterAppImage from './assets/images/gamesterAppImage.png';
 import CarCarLogo from './assets/images/CarCarLogo.png';
 import CarCarAppImage from './assets/images/CarCarAppImage.png';
+import GitLinks from "./GitLinks";
 
 function Home() {
     return (
@@ -31,8 +32,14 @@ function Home() {
                                 <h3>PROJECTS</h3>
                                 <section className="card-container">
                                     <img src={gamesterLogo} className="background-image" alt="Gamester Logo"/>
-                                    <img src={gamesterAppImage} className="overlay" alt="Gamester Screenshot" />
-                                </section>
+                                    <img src={gamesterAppImage} className="overlay" alt="Gamester Screenshot"/>
+                                    <div className="button-overlay">
+                                        <div className="app-date">April/2024 - May/2024</div>
+                                        <div className="app-description">GAMESTER - Create and manage tournaments, list a venue for hosting, as well as registering users for tournament participation.</div>
+                                        <div className="app-git"><GitLinks /></div>
+                                        <div className="app-tech">Python React FastAPI PostgreSQL CSS</div>
+                                    </div>
+                               </section>
                                 <section className="card-container">
                                     <img src={CarCarLogo} className="background-image" alt="Gamester Logo"/>
                                     <img src={CarCarAppImage} className="overlay" alt="Gamester Screenshot" />
